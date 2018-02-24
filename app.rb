e23183605b5cfb970088b145377268ea0735b52c
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-24 17:38:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,13 +23,14 @@ class App < Sinatra::Base
      "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get "/:operation/:number1/:number2" do 
+  get "/:operation/:number1/:number2" do
     operation = params[:operation]
     number1 = params[:number1].to_i
     number1 = params[:number2].to_i
-    
+
   end
 
+  
   
 
 
